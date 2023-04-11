47ec05cf1e513a5817d0f6a52b545e65c6a918d6
Matteo Maria Emanuele Cialei <matteome.cialei@gmail.com>
2023-04-11 08:20:43 +0200

    first commit and scaffolding

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <shapes/shapes.h>
+
+int main (int argc, char *argv[])
+{
+    
+    std::cout << Greet("Matteo") << std::endl;
+    return 0;
+}
---
+++ src/shapes.cpp
@@ -0,0 +1,7 @@
+#include <shapes/shapes.h>
+
+
+std::string Greet(std::string s)
+{
+    return "Hello " + s + "!";
+}
